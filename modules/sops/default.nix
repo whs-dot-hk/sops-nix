683b57ab2380178d19935e2725d31ec2f0804075
whs <hswongac@gmail.com>
2024-09-20 22:53:30 +0800

    Test

--- modules/sops/default.nix
+++ modules/sops/default.nix
@@ -322,7 +322,7 @@ in {
   #    );
 
   config = {
-      sops.environment.SOPS_GPG_EXEC = lib.mkIf (cfg.gnupg.home != null || cfg.gnupg.sshKeyPaths != []) (lib.mkDefault "${pkgs.gnupg}/bin/gpg");
+      #sops.environment.SOPS_GPG_EXEC = lib.mkIf (cfg.gnupg.home != null || cfg.gnupg.sshKeyPaths != []) (lib.mkDefault "${pkgs.gnupg}/bin/gpg");
 
       # When using sysusers we no longer be started as an activation script because those are started in initrd while sysusers is started later.
       systemd.services.sops-install-secrets = {
@@ -340,6 +340,7 @@ in {
           RemainAfterExit = true;
         };
       };
+    };
 
       #system.activationScripts = {
       #  setupSecrets = lib.mkIf (regularSecrets != {} && !useSystemdActivation) (lib.stringAfter ([ "specialfs" "users" "groups" ] ++ lib.optional cfg.age.generateKey "generate-age-key") ''
@@ -360,9 +361,9 @@ in {
       #    fi
       #  '');
       #};
-    })
-    {
-     system.build.sops-nix-manifest = manifest;
-    }
+    #})
+    #{
+    # system.build.sops-nix-manifest = manifest;
+    #}
   ];
 }
